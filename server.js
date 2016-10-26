00f89ed4d717a51d2fd0eebcf4a26df084497aa1
Pavel Zubkou <pavel.zubkou@instinctools.ru>
2016-10-26 11:54:56 +0300

    Extract app.js from server.js

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+const app = require('./app');
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
